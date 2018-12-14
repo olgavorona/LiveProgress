fce608e7ff65c7dccd3821d359e9e8e00da22f98
Olga Vorona <olgavorona1@gmail.com>
2018-12-14 15:46:42 +0700

    Initial Commit

---
+++ LiveProgress/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LiveProgress
+//
+//  Created by Olga Vorona on 14/12/2018.
+//  Copyright © 2018 Olga Vorona. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
